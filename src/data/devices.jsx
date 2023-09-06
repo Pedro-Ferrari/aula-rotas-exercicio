53bfd547eb0346d59bc704f1225dbbb8dea41da0
Pedro-Ferrari <pedrofcmp@gmail.com>
2023-09-06 20:11:42 -0300

    device atualizado.

--- src/data/devices.jsx
+++ src/data/devices.jsx
@@ -3,7 +3,8 @@ const devices = [
       id: 1,
       name: 'Smartphone 1',
       image: 'smartphone1.jpg',
-      
+      description: 'Descrição do Smartphone 1.',
+      price: 599.99,
     },
     // Outros dispositivos...
   ];
