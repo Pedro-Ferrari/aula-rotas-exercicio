8f5db7b34c7ed3d4fa4fd8ded6f8278d335dc260
Pedro-Ferrari <pedrofcmp@gmail.com>
2023-09-06 20:11:05 -0300

    Data criado

---
+++ src/data/devices.jsx
@@ -0,0 +1,12 @@
+const devices = [
+    {
+      id: 1,
+      name: 'Smartphone 1',
+      image: 'smartphone1.jpg',
+      
+    },
+    // Outros dispositivos...
+  ];
+  
+  export default devices;
+  
