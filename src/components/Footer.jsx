c8801b7bb39d4c9dce4223cac052026cf465ed00
Pedro-Ferrari <pedrofcmp@gmail.com>
2023-09-06 20:44:12 -0300

    Footer atualizado

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
   return (
     <footer>
-      <p>&copy; {new Date().getFullYear()} Nome da Sua Loja. Todos os direitos reservados.</p>
+      <p>&copy; {new Date().getFullYear()} CelFree. Todos os direitos reservados.</p>
     </footer>
   );
 }
